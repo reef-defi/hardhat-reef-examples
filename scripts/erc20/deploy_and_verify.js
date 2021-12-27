7b9fd04427326303bbe7948a27f30bcc255f82d4
Žiga Franko <franko.ziga@gmail.com>
2021-12-27 21:24:25 +0100

    Deploy and verify (#7)
    * reef hardhat update
    
    * deploy and verify flipper and erc20 token
    
    Co-authored-by: Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>

---
+++ scripts/erc20/deploy_and_verify.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+
+async function main() {
+  // We will deploy Token contract with Bob
+  // It is going to have the pool of 1000000 tokens
+  const acc = await hre.reef.getSignerByName("testnet_account");
+  const Token = await hre.reef.getContractFactory("Token", acc);
+  const args = [1000000]
+  const token = await Token.deploy(1000000);
+
+  await token.deployed();
+  console.log("Deploy done");
+  console.log({
+    token: token.address,
+  });
+  console.log({
+    name: await token.name(),
+    initialBalance: await token.totalSupply(),
+  });
+  await hre.reef.verifyContract(token.address, "Token", args);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
