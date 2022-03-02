2abc3274b60a829e02b88668d80a5bdadfbac830
Furion <_@furion.me>
2022-03-02 14:53:14 +0100

    Update default token name

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-		constructor(uint256 initialBalance) public ERC20("Reef", "REEF") {
+		constructor(uint256 initialBalance) public ERC20("Test Token", "TEST") {
 				_mint(msg.sender, initialBalance);
 		}
 }
